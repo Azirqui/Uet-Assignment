e66abdbe03660897ae82cc7b874cef36f503f0a1
Chaudhary Zeeshan Ahmad <mr.noman0095@gmail.com>
2022-12-18 18:11:59 +0500

    This is Assignment of PD

---
+++ PD Week 2/Tasks/Project/Task07(CP).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+system("Color 17");
+cout <<"                        .::---::..                                  "<<endl;
+cout <<"                     .-------------.                                "<<endl;
+cout <<"                   .-----------------.                              "<<endl;
+cout <<"                   ----------------:.                               "<<endl;
+cout <<"                  :-----------::.                                   "<<endl;
+cout <<"                  -----------:.                                     "<<endl;
+cout <<"                  :-------------:..                                 "<<endl;
+cout <<"                   :----------------:.                              "<<endl;
+cout <<"                    .---------------:                               "<<endl;
+cout <<"                      .:---------:.                                 "<<endl;
+
+
+}
---
+++ PD Week 2/Tasks/Task03(CP).txt
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout <<". A "<<endl;
+cout <<". X "<<endl;
+cout <<". Z "<<endl;
+cout <<". K "<<endl;
+cout <<". S "<<endl;
+cout <<". U "<<endl;
+
+}
